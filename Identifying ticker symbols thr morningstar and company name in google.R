8af77691e3cb0970bdd07e5eadf4426c4efce9f9
HUANG JIRONG <jironghuang88@gmail.com>
2017-10-26 15:05:41 +0700

    Add files via upload

---
+++ Identifying ticker symbols thr morningstar and company name in google.R
@@ -0,0 +1,56 @@
+#Try scraping based on google search
+#Ticker symbol before --> </a> <span class="_RWc
+#And stock exchange after that
+
+library("stringr")
+library("XML")  #for parsing
+library("rvest")
+library("RCurl")
+library("XML")
+
+
+#functions to locate start and end of name
+#functions in the main function
+locate_start = function(x,pat){
+  end = str_locate_all(pattern = pat,x)[[1]][1,1]
+  return (end)
+}
+
+locate_end = function(x,pat){
+  end = str_locate_all(pattern = pat,x)[[1]][1,2]
+  return (end)
+}
+
+setwd("C:/Users/HUANGJ/Desktop/items/Rewards research/Paynet")
+coy = read.csv("de_deup_parent_coy_ticker_symbol.csv", stringsAsFactors = FALSE)
+
+coy$format_name = gsub("[[:punct:]]", "", coy$ClientName_parent)
+coy$format_name = gsub("( )+", "+", coy$format_name)
+# https://www.google.com.sg/search?q=berkshire+hathaway
+coy$google_search = paste("https://www.google.com.sg/search?q=",coy$format_name,"+morningstar",sep = "")
+coy$ticker = ""
+coy$exchange = ""
+
+for(i in 1:nrow(coy)){
+  print(i);print(coy$google_search[i])
+  html = getURL(coy$google_search[i], ssl.verifypeer = FALSE)
+  # a = as.data.frame(html); a$html = as.character(a$html)
+  
+  if(grepl('Stock Quote Price News',html)){
+    
+    ticker = str_locate(html,'Stock Quote Price News')[1,1]
+
+    coy$ticker[i] = substring(html, first = ticker - 50, last = ticker+20)  
+    # fortune100$descr[i] = substring(fortune100$descr[i], first = str_locate(fortune100$descr[i],"<p>")+3, last = -1 + str_locate(fortune100$descr[i],"</p>")) 
+    
+  }
+}  
+
+
+
+
+
+
+
+
+
